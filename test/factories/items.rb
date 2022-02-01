93f5b5b9f02c79046ec1e076f717ba4db44621b9
aspahn99 <37812253+aspahn99@users.noreply.github.com>
2022-02-01 12:01:17 -0500

    Add files via upload

---
+++ test/factories/items.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :item do
+    name "Honey Wheat Bread"
+    description "A most tasty treat from the bakers at Bread Express. Your family will love it!"
+    # picture nil
+    category "bread"
+    units_per_item 1
+    weight 1.0
+    active true
+    picture nil
+  end
+end
